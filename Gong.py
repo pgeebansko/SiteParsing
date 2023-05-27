9e9faf563b5a162603902ad6f38fcbaed5195cf4
pgeebansko <pgee_students@abv.bg>
2023-05-28 00:54:31 +0300

    v1 - 2

--- Gong.py
+++ Gong.py
@@ -35,22 +35,27 @@ def read_time(txt):
 
 
 def check_time(txt):
-    p = row.find('span', 'hour')
+    p = txt.find('div', 'status')
     s = str(type(p))
-    result = ''
     if s != "<class 'NoneType'>":
-        p = p.text
-        lines = p.splitlines()
-        s = lines[1].lstrip().rstrip()
-        s = s.rpartition(', ')
-        match_date = s[0]
-        match_time = s[2]
-        if match_time < '22:00':
-            return True
+        return False
+    else:
+        p = row.find('span', 'hour')
+        s = str(type(p))
+        result = ''
+        if s != "<class 'NoneType'>":
+            p = p.text
+            lines = p.splitlines()
+            s = lines[1].lstrip().rstrip()
+            s = s.rpartition(', ')
+            match_date = s[0]
+            match_time = s[2]
+            if match_time < '22:00':
+                return True
+            else:
+                return False
         else:
             return False
-    else:
-        return False
 
 
 url = "https://gong.bg/livescore"
@@ -63,10 +68,11 @@ tables = soup.findAll('div', class_='ls-schedule-table')
 for table_html in tables:
     title = table_html.find('a', 'gtm-Livescore').find('h2').text
     match_rows = table_html.findAll('div', 'match-row')
-    if (len(match_rows) > 0) and check_time(row):
+    if len(match_rows) > 0:
         print(title)
         for row in match_rows:
             host = row.find('div', 'host').text
             guest = row.find('div', 'guest').text
-            print(f'      {host} - {guest}   ({read_status(row)}) ')
+            if check_time(row):
+                print(f'      {host} - {guest}   ({read_status(row)}) ')
 
